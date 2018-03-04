369bcf9f622a047535e347365f8ca7564306786e
alfedd@crimson.ua.edu Fedd <alfedd@crimson.ua.edu>
2018-03-04 23:49:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // write your code below!
+var function = (holiday,name);
+function happyHolidays(string) {
+  return "Happy holidays!"
+}
+function happyHolidaysTo(string) {
+  return `Happy holidays, you!`
+}
+
