7d64a94bd876022787b0727977d39935d6530f54
alfedd@crimson.ua.edu Fedd <alfedd@crimson.ua.edu>
2018-03-04 23:54:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function happyHolidayTo(string) {
   return "Happy Independence Day, you!"
 }
 function holidayCountdown(string) {
-  return "It's 20 days until Mother's Day"
+  return "It's 20 days until Mother's Day!"
 }
